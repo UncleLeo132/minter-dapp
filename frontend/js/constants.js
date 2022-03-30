2f3f81437ad9afc7fdc518fd710862b8a5821b62
UncleLeo132 <leo132oel@gmail.com>
2022-03-30 13:40:07 +0400

    n

--- frontend/js/constants.js
+++ frontend/js/constants.js
@@ -3,7 +3,7 @@ const chain = 'polygon'; // rinkeby or polygon
 
 const welcome_h1 = "Welcome to the Confident Bears NFT Project!!";
 const welcome_h2 = "Connect to MetaMask to Get Started";
-const welcome_p = 'The Confident Bears NFT Project owner will get whitelist spots as well as many Exlusive rights im the Aureus project';
+const welcome_p = 'The Confident Bears NFT Project owner will get whitelist spots as well as many Exlusive rights in the Aureus project';
 const h1_presale_coming_soon = "NFT Drop Coming Soon!!";
 const h1_presale_mint = "Pre-Sale Minting Open!!";
 const h1_public_mint = "Public Minting Open!!";
